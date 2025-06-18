f5ffe51785442998e55715c45b3e665f03addb90
Bryce Mecum <petridish@gmail.com>
2025-06-18 11:18:58 -0700

    Change help text for list command

--- cmd/dbc/main.go
+++ cmd/dbc/main.go
@@ -22,7 +22,7 @@ type modelCmd interface {
 
 func main() {
 	var args struct {
-		List    *ListCmd       `arg:"subcommand" help:"Fetch list of drivers"`
+		List    *ListCmd       `arg:"subcommand" help:"List available drivers"`
 		Config  *ViewConfigCmd `arg:"subcommand" help:"View driver config"`
 		Install *InstallCmd    `arg:"subcommand" help:"Install driver"`
 		Tui     *TuiCmd        `arg:"subcommand"`
