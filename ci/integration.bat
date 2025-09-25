b3d3c2167aa4cc1e9c9231a1cfd55d83f4f1e496
Bryce Mecum <petridish@gmail.com>
2025-09-24 21:34:29 -0700

    this should work

--- ci/integration.bat
+++ ci/integration.bat
@@ -5,7 +5,6 @@ python -m venv .venv
 call ".venv\Scripts\activate.bat"
 pip install adbc_driver_manager
 
-set PATH=%CD%;%PATH%
-.\dbc install duckdb
+dbc.exe install duckdb
 
 python -c "from adbc_driver_manager import dbapi; dbapi.connect(driver=\"duckdb\");"
