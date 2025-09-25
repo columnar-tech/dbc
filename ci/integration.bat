d842e2ffec35c10653c07089a3294448d1668ca2
Bryce Mecum <petridish@gmail.com>
2025-09-24 21:22:49 -0700

    PATH?

--- ci/integration.bat
+++ ci/integration.bat
@@ -5,6 +5,7 @@ python -m venv .venv
 call ".venv\Scripts\activate.bat"
 pip install adbc_driver_manager
 
+set PATH=%CD%;%PATH%
 dbc install duckdb
 
 python -c "from adbc_driver_manager import dbapi; dbapi.connect(driver=\"duckdb\");"
