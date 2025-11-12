1c270e6375c023d90b5f5707f24ba4189d45bee3
Matt Topol <zotthewizard@gmail.com>
2025-11-12 18:46:21 -0500

    next test

--- cmd/duckdbtest/main.go
+++ cmd/duckdbtest/main.go
@@ -3,32 +3,29 @@ package main
 import (
 	"context"
 	"fmt"
-	"os"
-	"os/exec"
-	"path/filepath"
 
 	"github.com/apache/arrow-adbc/go/adbc/drivermgr"
 )
 
-func installDuckDBWithDbc(ctx context.Context) error {
-	wd, err := os.Getwd()
-	if err != nil {
-		return fmt.Errorf("failed to get working directory: %v", err)
-	}
-	installCmd := exec.CommandContext(ctx, filepath.Join(wd, "dbc"), "install", "duckdb")
-	output, err := installCmd.CombinedOutput()
-	if err != nil {
-		return fmt.Errorf("failed to install duckdb driver: %v, output: %s", err, string(output))
-	}
-	fmt.Println(string(output))
-	return nil
-}
+// func installDuckDBWithDbc(ctx context.Context) error {
+// 	wd, err := os.Getwd()
+// 	if err != nil {
+// 		return fmt.Errorf("failed to get working directory: %v", err)
+// 	}
+// 	installCmd := exec.CommandContext(ctx, filepath.Join(wd, "dbc"), "install", "duckdb")
+// 	output, err := installCmd.CombinedOutput()
+// 	if err != nil {
+// 		return fmt.Errorf("failed to install duckdb driver: %v, output: %s", err, string(output))
+// 	}
+// 	fmt.Println(string(output))
+// 	return nil
+// }
 
 func main() {
 	ctx := context.Background()
-	if err := installDuckDBWithDbc(ctx); err != nil {
-		fmt.Println("Error:", err)
-	}
+	// if err := installDuckDBWithDbc(ctx); err != nil {
+	// 	fmt.Println("Error:", err)
+	// }
 
 	var drv drivermgr.Driver
 	db, err := drv.NewDatabase(map[string]string{
