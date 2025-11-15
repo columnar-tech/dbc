106e5c3647628c1f7e1f46d76233dfe778ea06e6
Bryce Mecum <petridish@gmail.com>
2025-11-14 16:42:15 -0800

    testing comparing the k handle with constants

--- config/dirs_windows.go
+++ config/dirs_windows.go
@@ -47,17 +47,6 @@ func (c ConfigLevel) key() registry.Key {
 	}
 }
 
-func (c ConfigLevel) rootKeyString() string {
-	switch c {
-	case ConfigUser:
-		return "HKEY_CURRENT_USER"
-	case ConfigSystem:
-		return "HKEY_LOCAL_MACHINE"
-	default:
-		return "UNKNOWN"
-	}
-}
-
 func (c ConfigLevel) configLocation() string {
 	var prefix string
 	switch c {
@@ -156,7 +145,16 @@ func driverInfoFromKey(k registry.Key, driverName string, lvl ConfigLevel) (di D
 
 	// For drivers in the registry, set FilePath to the registry key instead
 	// of the filesystem path since that's technically where the driver exists.
-	di.FilePath = fmt.Sprintf("%s\\%s", lvl.rootKeyString(), regKeyADBC)
+	var rootKey string
+	switch k {
+	case registry.CURRENT_USER:
+		rootKey = "HKCU"
+	case registry.LOCAL_MACHINE:
+		rootKey = "HKLM"
+	default:
+		rootKey = "UNK"
+	}
+	di.FilePath = fmt.Sprintf("%s\\%s", rootKey, regKeyADBC)
 
 	return
 }
