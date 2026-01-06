d627b36df685cc5c927b02d109ba2c10809f7f64
Bryce Mecum <petridish@gmail.com>
2026-01-05 19:10:40 -0800

    feat: support getting docs urls from driver registry index (#221)
    Implements a proper solution for `dbc docs` to get a URL for a driver's
    docs when the user runs `dbc docs $driver_path`. The previous
    implementation used a hardcoded set of fallback URLs just to get
    something working. This feature works gracefully with the current driver
    registry index structure but can take advantage of a new "docs_url" if
    one is present.
    
    This PR also updates the fallback URLs map so every driver now works
    with `dbc docs`.
    
    Closes #220

--- cmd/dbc/search_test.go
+++ cmd/dbc/search_test.go
@@ -28,7 +28,8 @@ func (suite *SubcommandTestSuite) TestSearchCmd() {
 		"• test-driver-2 - This is another test driver\n"+
 		"• test-driver-manifest-only - This is manifest-only driver\n"+
 		"• test-driver-no-sig - Driver manifest missing Files.signature entry\n"+
-		"• test-driver-invalid-manifest - This is test driver with an invalid manifest. See https://github.com/columnar-tech/dbc/issues/37.\n", suite.runCmd(m))
+		"• test-driver-invalid-manifest - This is test driver with an invalid manifest. See https://github.com/columnar-tech/dbc/issues/37.\n"+
+		"• test-driver-docs-url - This is manifest-only with its docs_url key set\n", suite.runCmd(m))
 }
 
 func (suite *SubcommandTestSuite) TestSearchCmdWithInstalled() {
@@ -43,7 +44,8 @@ func (suite *SubcommandTestSuite) TestSearchCmdWithInstalled() {
 		"• test-driver-2 - This is another test driver\n"+
 		"• test-driver-manifest-only - This is manifest-only driver\n"+
 		"• test-driver-no-sig - Driver manifest missing Files.signature entry\n"+
-		"• test-driver-invalid-manifest - This is test driver with an invalid manifest. See https://github.com/columnar-tech/dbc/issues/37.\n", suite.runCmd(m))
+		"• test-driver-invalid-manifest - This is test driver with an invalid manifest. See https://github.com/columnar-tech/dbc/issues/37.\n"+
+		"• test-driver-docs-url - This is manifest-only with its docs_url key set\n", suite.runCmd(m))
 }
 
 func (suite *SubcommandTestSuite) TestSearchCmdVerbose() {
@@ -64,6 +66,9 @@ func (suite *SubcommandTestSuite) TestSearchCmdVerbose() {
 		"Available Versions:\n    ╰── 1.0.0\n"+
 		"• test-driver-invalid-manifest\n   Title: Test Driver Invalid Manifest\n   "+
 		"Description: This is test driver with an invalid manifest. See https://github.com/columnar-tech/dbc/issues/37.\n   License: Apache-2.0\n   "+
+		"Available Versions:\n    ╰── 1.0.0\n"+
+		"• test-driver-docs-url\n   Title: Test Driver With Docs URL Set\n   "+
+		"Description: This is manifest-only with its docs_url key set\n   License: Apache-2.0\n   "+
 		"Available Versions:\n    ╰── 1.0.0\n", suite.runCmd(m))
 }
 
@@ -97,6 +102,12 @@ func (suite *SubcommandTestSuite) TestSearchCmdVerboseWithInstalled() {
 		"• test-driver-invalid-manifest\n"+
 		"   Title: Test Driver Invalid Manifest\n"+
 		"   Description: This is test driver with an invalid manifest. See https://github.com/columnar-tech/dbc/issues/37.\n"+
+		"   License: Apache-2.0\n"+
+		"   Available Versions:\n"+
+		"    ╰── 1.0.0\n"+
+		"• test-driver-docs-url\n"+
+		"   Title: Test Driver With Docs URL Set\n"+
+		"   Description: This is manifest-only with its docs_url key set\n"+
 		"   License: Apache-2.0\n"+
 		"   Available Versions:\n"+
 		"    ╰── 1.0.0\n", suite.runCmd(m))
