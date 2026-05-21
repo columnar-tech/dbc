3de7421aad259224c409aa64e432a3ba30a37f33
Bryce Mecum <petridish@gmail.com>
2026-05-21 07:47:02 -0700

    fix: try to enable vt or disable colors on startup on Windows (#383)
    Fixes an issue a user reported privately where they were seeing ANSI
    escapes in dbc's output. I was able to reproduce their issue by setting
    `HKCU\Console\VirtualTerminalLevel` to `1` and I came up with this as a
    fix. When dbc starts on Windows, try to enable VT processing if it's not
    on and, if we fail, set `NO_COLOR` so the user doesn't get garbled
    output. I wasn't able to figure out how an admin could disable VT
    processing so that branch wasn't tested.
    
    ---------
    
    Co-authored-by: Matt Topol <zotthewizard@gmail.com>

---
+++ cmd/dbc/terminal_windows.go
@@ -0,0 +1,67 @@
+// Copyright 2026 Columnar Technologies Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+//go:build windows
+
+package main
+
+import (
+	"os"
+
+	"golang.org/x/sys/windows"
+)
+
+const (
+	ENABLE_VIRTUAL_TERMINAL_PROCESSING = 0x0004
+)
+
+func initVTProcessing() {
+	if !enableVTProcessing(os.Stdout) || !enableVTProcessing(os.Stderr) {
+		disableColors()
+		return
+	}
+}
+
+// enableVTProcessing attempts to enable ENABLE_VIRTUAL_TERMINAL_PROCESSING
+// on the given file handle. Returns true if successful or already enabled.
+func enableVTProcessing(f *os.File) bool {
+	handle := windows.Handle(f.Fd())
+	var mode uint32
+
+	// Get current console mode
+	err := windows.GetConsoleMode(handle, &mode)
+	if err != nil {
+		return false
+	}
+
+	// Check if VT processing is already enabled
+	if mode&ENABLE_VIRTUAL_TERMINAL_PROCESSING != 0 {
+		return true
+	}
+
+	// Try to enable VT processing
+	newMode := mode | ENABLE_VIRTUAL_TERMINAL_PROCESSING
+	err = windows.SetConsoleMode(handle, newMode)
+	if err != nil {
+		return false
+	}
+
+	return true
+}
+
+// disableColors sets the NO_COLOR environment variable to disable ANSI colors.
+// lipgloss and colorprofile respect NO_COLOR automatically.
+func disableColors() {
+	os.Setenv("NO_COLOR", "1")
+}
